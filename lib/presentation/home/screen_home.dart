2c5dffc5c533b6e241deb3d99b591c2a99f67bda
Adithya <adithyavinod12345@gmail.com>
2025-06-30 03:51:36 +0530

    Refactored Background card

--- lib/presentation/home/screen_home.dart
+++ lib/presentation/home/screen_home.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_netflix/core/colors/colors.dart';
 import 'package:flutter_netflix/core/constants.dart';
-import 'package:flutter_netflix/presentation/home/widgets/custom_button_widget.dart';
+import 'package:flutter_netflix/presentation/home/widgets/background_card.dart';
 import 'package:flutter_netflix/presentation/home/widgets/number_title_card.dart';
 import 'package:flutter_netflix/presentation/widgets/main_title_card.dart';
 
@@ -15,50 +14,7 @@ class ScreenHome extends StatelessWidget {
     return Scaffold(
       body: ListView(
         children: [
-          Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                height: 600,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(kMainImage),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                height: 600,
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Colors.black, Colors.black54, Colors.black12],
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.center,
-                  ),
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      CustomButtonWidget(title: "My List", icon: Icons.add),
-                      _playButton(),
-                      CustomButtonWidget(
-                        title: "Info",
-                        icon: Icons.info_outline,
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+          BackgroundCard(),
           MainTitleCard(homeTitle: "Released in the Past Year"),
           kHeight10,
           MainTitleCard(homeTitle: "Trending Now"),
@@ -69,31 +25,6 @@ class ScreenHome extends StatelessWidget {
           MainTitleCard(homeTitle: "South Indian Cinema"),
         ],
       ),
-    );
-  }
-
-  TextButton _playButton() {
-    return TextButton.icon(
-      style: ButtonStyle(
-        // ignore: deprecated_member_use
-        shape: MaterialStateProperty.all(
-          RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-        ),
-        // ignore: deprecated_member_use
-        backgroundColor: MaterialStateProperty.all(kWhiteColor),
-        iconColor: WidgetStateProperty.all(kBlackColor),
-      ),
-      onPressed: () {},
-      label: Text(
-        "Play",
-        style: TextStyle(
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-          color: kBlackColor,
-        ),
-      ),
-
-      icon: Icon(Icons.play_arrow, size: 30),
     );
   }
 }
---
+++ lib/presentation/home/widgets/background_card.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_netflix/core/colors/colors.dart';
+import 'package:flutter_netflix/core/constants.dart';
+import 'package:flutter_netflix/presentation/home/widgets/custom_button_widget.dart';
+
+class BackgroundCard extends StatelessWidget {
+  const BackgroundCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 600,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(kMainImage),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          height: 600,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.black, Colors.black54, Colors.black12],
+              begin: Alignment.bottomCenter,
+              end: Alignment.center,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                CustomButtonWidget(title: "My List", icon: Icons.add),
+                _playButton(),
+                CustomButtonWidget(title: "Info", icon: Icons.info_outline),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  TextButton _playButton() {
+    return TextButton.icon(
+      style: ButtonStyle(
+        // ignore: deprecated_member_use
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        ),
+        // ignore: deprecated_member_use
+        backgroundColor: MaterialStateProperty.all(kWhiteColor),
+        iconColor: WidgetStateProperty.all(kBlackColor),
+      ),
+      onPressed: () {},
+      label: Text(
+        "Play",
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+          color: kBlackColor,
+        ),
+      ),
+
+      icon: Icon(Icons.play_arrow, size: 30),
+    );
+  }
+}
